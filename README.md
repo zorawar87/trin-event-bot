6cdc0ed195613504ac5609f81b8f986575c2b527
Zorawar Moolenaar <zorawar87@gmail.com>
2018-08-31 10:18:35 -0400

    added travis build status

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+![Travis Build Status](https://travis-ci.org/zorawar87/trin-event-bot.svg?branch=master)
 # trin-event-bot
 a twitter bot that tweets events on trinity college campus
--- main.go
+++ main.go
@@ -8,10 +8,6 @@ import (
 	"time"
 )
 
-type Rss struct {
-	Channels []Channel `xml:"channel"`
-}
-
 /*
 <title>All Events</title>
 <description>Trinity College Events</description>
@@ -22,15 +18,17 @@ type Rss struct {
 <generator>LiveWhale 1.6.2</generator>
 <atom:link href="https://events.trincoll.edu/live/rss/events/header/All%20Events" rel="self" type="application/rss+xml"/>
 */
-type Channel struct {
-	Title       string  `xml:"title"`
-	Description string  `xml:"description"`
-	Link        string  `xml:"link"`
-	Category    string  `xml:"category"`
-	Events      []Event `xml:"item"`
+
+// Represents part of the structure of the XML page representing events at Trincoll
+type Result struct {
+	Title       string  `xml:"channel>title"`
+	Description string  `xml:"channel>description"`
+	Link        string  `xml:"channel>link"`
+	Category    string  `xml:"channel>category"`
+	Events      []Event `xml:"channel>item"`
 }
 
-func (c Channel) String() string {
+func (c Result) String() string {
 	return fmt.Sprintf("%s [%s] (%s) Link:<%s> \nEvents:\n%s", c.Title, c.Category, c.Description, c.Link, c.Events)
 }
 
@@ -48,6 +46,7 @@ func (c Channel) String() string {
 	<source url="https://events.trincoll.edu/live/rss/events/header/All%20Events">All Events</source>
 </item>
 */
+// Represents a single event being held at trincoll
 type Event struct {
 	Title     string `xml:"title"`
 	Link      string `xml:"link"`
@@ -66,8 +65,9 @@ func main() {
 	defer resp.Body.Close()
 	bytes, _ := ioutil.ReadAll(resp.Body)
 
-	var events Rss
+	var events Result
 	xml.Unmarshal(bytes, &events)
+	fmt.Println(events)
 
 	t, err := time.Parse(time.RFC1123Z, "Tue, 26 Feb 2019 23:30:00 +0000")
 	panicOnErr(err)
