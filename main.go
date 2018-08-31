da2b9e9f2b36456e1bc94aed4aeafdf0dcf53b0e
Zorawar Moolenaar <zorawar87@gmail.com>
2018-08-31 17:47:58 -0400

    can parse published events; partially ready to use programmatically

--- main.go
+++ main.go
@@ -1,7 +1,81 @@
 package main
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
 
-func main(){
-  fmt.Println("Hello")
+type Rss struct {
+	Channels []Channel `xml:"channel"`
+}
+
+/*
+<title>All Events</title>
+<description>Trinity College Events</description>
+<link>https://events.trincoll.edu/live/rss/events/header/All%20Events</link>
+<category domain="https://events.trincoll.edu/">events</category>
+<language>en-us</language>
+<ttl>60</ttl>
+<generator>LiveWhale 1.6.2</generator>
+<atom:link href="https://events.trincoll.edu/live/rss/events/header/All%20Events" rel="self" type="application/rss+xml"/>
+*/
+type Channel struct {
+	Title       string  `xml:"title"`
+	Description string  `xml:"description"`
+	Link        string  `xml:"link"`
+	Category    string  `xml:"category"`
+	Events      []Event `xml:"item"`
+}
+
+func (c Channel) String() string {
+	return fmt.Sprintf("%s [%s] (%s) Link:<%s> \nEvents:\n%s", c.Title, c.Category, c.Description, c.Link, c.Events)
+}
+
+/*
+<item>
+	<title>First-Year Shabbat</title>
+	<link>https://events.trincoll.edu/#!view/event/event_id/61813</link>
+	<pubDate>Fri, 31 Aug 2018 22:00:00 +0000</pubDate>
+	<guid isPermaLink="true">https://events.trincoll.edu/#!view/event/event_id/61813#61813</guid>
+	<livewhale:type>events</livewhale:type>
+	<livewhale:id>61813</livewhale:id>
+	<livewhale:timezone>America/New_York</livewhale:timezone>
+	<livewhale:categories>Religious Service</livewhale:categories>
+	<livewhale:ends>Fri, 31 Aug 2018 23:30:00 +0000</livewhale:ends>
+	<source url="https://events.trincoll.edu/live/rss/events/header/All%20Events">All Events</source>
+</item>
+*/
+type Event struct {
+	Title     string `xml:"title"`
+	Link      string `xml:"link"`
+	Type      string `xml:"http://www.livewhale.com/ type"`
+	Category  string `xml:"http://www.livewhale.com/ categories"`
+	StartsUtc string `xml:"pubDate"`
+}
+
+func (e Event) String() string {
+	return fmt.Sprintf("[%s, %s] %-20s <%-10s> at %-15s\n", e.Category, e.Type, e.Title, e.Link, e.StartsUtc)
+}
+
+func main() {
+	resp, err := http.Get("https://events.trincoll.edu/live/rss/events/header/All Events")
+	panicOnErr(err)
+	defer resp.Body.Close()
+	bytes, _ := ioutil.ReadAll(resp.Body)
+
+	var events Rss
+	xml.Unmarshal(bytes, &events)
+
+	t, err := time.Parse(time.RFC1123Z, "Tue, 26 Feb 2019 23:30:00 +0000")
+	panicOnErr(err)
+	fmt.Println(t.Local())
+}
+
+func panicOnErr(e error) {
+	if e != nil {
+		panic(e.Error())
+	}
 }
